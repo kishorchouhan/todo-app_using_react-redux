b3fcdff26361cef7293d016449196952c440d96a
kishorchouhan <devkishorbwr@gmail.com>
2019-07-17 18:09:31 +0530

    Base code.

---
+++ src/components/TodoList/TodoList.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import "./TodoList.css";
+
+export default class TodoList extends Component {
+  render() {
+    return (
+      <div className="todo-list">
+        <h1>TODOS</h1>
+      </div>
+    );
+  }
+}
